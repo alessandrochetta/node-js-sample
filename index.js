500dcd9870c6653f1922125912b9002a910e4946
EC2 Default User <ec2-user@ip-172-31-11-195.us-west-2.compute.internal>
2017-01-23 06:43:44 +0000

    server updated

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express()
+var path = require("path")
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'public/home', 'index.html'))
+  console.log(path.join(__dirname, 'public/home/', 'index.html'))
+  response.sendFile(path.join(__dirname, 'public/home/', 'index.html'))
 })
 
 app.listen(app.get('port'), function() {
